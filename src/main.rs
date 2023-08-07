bcabfafe8e96121cd18cbb47259edc039a418560
s19140 <s19140@numazu.kosen-ac.jp>
2023-08-07 18:45:56 +0900

    プロジェクトの生成

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+//! not_leonian_ac_lib/main.rs
+//! Copyright (c) 2023 Not_Leonian
+//! This software is released under the MIT License.
+//! http://opensource.org/licenses/mit-license.php
+
+#[allow(unused_imports)]
+use proconio::{input,input_interactive};
+
+fn main() {
+    input!();
+}
