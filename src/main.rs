812deeff36a8fb4b246d7bc990e66c3947a86a0a
s19140 <s19140@numazu.kosen-ac.jp>
2023-08-08 21:35:39 +0900

    inputマクロ

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-//! not_leonian_ac_lib/main.rs
-//! Copyright (c) 2023 Not_Leonian
-//! This software is released under the MIT License.
-//! http://opensource.org/licenses/mit-license.php
-
-#[allow(unused_imports)]
-use proconio::{input,input_interactive};
+//! <https://github.com/NotLeonian/not_leonian_ac_lib>
 
 fn main() {
+    macro_rules! input {
+        ($($tt:tt)*) => {
+            proconio::input!($($tt)*);
+            // proconio::input_interactive!($($tt)*);
+        };
+    }
     input!();
 }
