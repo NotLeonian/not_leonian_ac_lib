09df06cd9b4104fabe662cfed26a27d91b6a30af
NotLeonian <leo_maru_gt@yahoo.co.jp>
2024-03-19 00:14:43 +0900

    main関数にallow(unstable_name_collisions)を追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_attributes)] #[macro_use] #[allow(unused_imports)] use not_leonian_ac_lib::*;
 
+#[allow(unstable_name_collisions)]
 fn main() {
     /// 標準入力のマクロ（インタラクティブ問題ならば中のマクロを変更）
     macro_rules! input {
